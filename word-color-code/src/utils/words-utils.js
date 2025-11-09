4fba5ed40b5d9403fe27f21342447eaf53635f05
Cyril Fleurot <cyril.fleurot@gmail.com>
2025-11-09 22:11:59 +1300

    Generate the HTML for order page

--- word-color-code/src/utils/words-utils.js
+++
@@ -1,4 +0,0 @@
-export function pickWord(wordList, wordLength) {
-	const filteredWords = wordList.filter(word => word.length === wordLength);
-	return filteredWords[Math.floor(Math.random() * filteredWords.length)];
-}
